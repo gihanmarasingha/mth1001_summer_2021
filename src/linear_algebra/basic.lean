80b135cf0d4fa71a01303aac57f1d617a0ddaefa
James Arthur <jamesarthur7@icloud.com>
2021-06-12 14:29:28 +0100

    added docstrings

--- src/linear_algebra/basic.lean
+++ src/linear_algebra/basic.lean
@@ -14,20 +14,31 @@ class vector_space (F : Type u) (V : Type v) [field F] [add_comm_group V] extend
 (smul_smul : ∀ (a b : F) (u : V), a • (b • u) = (a * b) • u)
 (smul_id : ∀ (u : V), (1 : F) • u = u)
 
+
+/-
+  Now we to define what we are working on, so let us define a field, an additive
+  abelian group (a group thats additive and commutative) and then a vector space.
+  Now, three elements of the vectorspace `u`, `v` and `w` and then `a`, `b` and `c`
+  elements of a field.
+-/
 variables [field F] [add_comm_group V] [vector_space F V] (u v w : V) (a b c : F)
 
 open vector_space
 
+
+/- Now prove the fact that 0 • u = 0, where 0 is the zero vector  -/
 lemma zero_smul' (u : V) : (0 : V) = (0 : F) • u :=
 begin
   sorry
 end
 
+/- What about the other way? -/
 lemma smul_zero'' (u : V) : a • (0 : V) = (0 : V) :=
 begin
   sorry
 end
 
+/- This one is slightly harder, try to prove that -u is just -1 • u -/
 lemma inv_eq_neg (u : V) (x : F) : -u = (-1 : F) • u :=
 begin
   sorry
